c1230d7bd629c1edda9bbf957fdd0289c3290694
AnijitSau <2anijit@gmail.com>
2020-02-20 19:03:09 +0530

    directory structure improved

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactSimpleDatePicker from './ReactSimpleDatePicker'
+
+ReactDOM.render(<ReactSimpleDatePicker />, document.getElementById('root'));
