84033df65fb0f59cdcb9bdd173be24db7526b09e
anijit <anijitsau@gmail.com>
2022-01-08 19:16:55 +0530

    all dependencies are up to date

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  devtool: "source-map",
+  output: {
+    filename: "ReactSimpleDatepicker.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  target: "web",
+}
